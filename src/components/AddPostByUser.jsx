18cedba976f37eb973e1004c2eebff22232885e0
Rubaalnadi <rubaalnadi5@gmail.com>
2022-06-01 15:25:04 +0300

    edit add post  component

--- src/components/AddPostByUser.jsx
+++ src/components/AddPostByUser.jsx
@@ -1,9 +1,18 @@
 import "../App.css";
 import React, { useEffect, useState } from "react";
 import EditUserInfo from "./EditUserInfo";
-function AddPostByUser({ userId , setBodyOfPost , bodyOfPost , setTest , updatePost ,  setUpdatePost}) {
-  const [postId , setPostId] = useState();
-//  const [test , setTest] = useState();
+
+const AddPostByUser = ({
+  userId,
+  setBodyOfPost,
+  bodyOfPost,
+  setTest,
+  updatePost,
+  setUpdatePost,
+}) => {
+  const [postId, setPostId] = useState();
+  //  const [test , setTest] = useState();
+  // function to make http req post in the posts
   const addPost = () => {
     fetch("https://jsonplaceholder.typicode.com/posts", {
       method: "POST",
@@ -16,35 +25,36 @@ function AddPostByUser({ userId , setBodyOfPost , bodyOfPost , setTest , updateP
       },
     })
       .then((response) => response.json())
-      .then((data) =>{ 
+      .then((data) => {
         setBodyOfPost(data.com);
-          console.log(data.com);
-          console.log(data.userId);
+        console.log(data.com);
+        console.log(data.userId);
         //   prev => [prev + data]
-          setTest([data]);
-          setPostId(data.id);
+        setTest([data]);
+        setPostId(data.id);
         //   console.log(test);
-        }
-          );
+      });
   };
+  // function to make http req patch (update) in the posts
   const editPost = () => {
     fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`, {
-        method: 'PATCH',
-        body: JSON.stringify({
-          com: updatePost,
-        }),
-        headers: {
-          'Content-type': 'application/json; charset=UTF-8',
-        },
-      })
-        .then((response) => response.json())
-        .then((data) => {
-            setTest([data])
-        }
-        );
-    }
+      method: "PATCH",
+      body: JSON.stringify({
+        com: updatePost,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        setTest([data]);
+      });
+  };
+
   useEffect(addPost, []);
   useEffect(editPost, []);
+
   return (
     <div className="flex justify-end ">
       <br />
@@ -55,21 +65,21 @@ function AddPostByUser({ userId , setBodyOfPost , bodyOfPost , setTest , updateP
       />
       <button
         onClick={() => {
-          addPost()
+          addPost();
         }}
       >
         Add
       </button>
       <button
+        // try to make edit
         onClick={() => {
-          <EditUserInfo  updatePost={updatePost} setUpdatePost={setUpdatePost}/>
+          <EditUserInfo setUpdatePost={setUpdatePost} />;
         }}
       >
         update
       </button>
-        
     </div>
   );
-}
+};
 
 export default AddPostByUser;
