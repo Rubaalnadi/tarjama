0ec7b3208cd35dd01e1482566e4e4eb9b472fe26
Rubaalnadi <rubaalnadi5@gmail.com>
2022-06-01 15:26:45 +0300

    edit userPost  component

--- src/components/UserPost.jsx
+++ src/components/UserPost.jsx
@@ -2,13 +2,16 @@ import "../App.css";
 import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
-
-function UserPost({ userName, userId }) {
+const UserPost = ({ userName, userId }) => {
   const navigate = useNavigate();
+  //state use to get post of user from api to know the number of it
   const [userPost, setUserPost] = useState([]);
-  let numberOfPost = userPost.length;
+  // to add length of user post state
+  let numberOfPost = userPost.length - 1;
+  // the same thing of albums
   const [numberOfAlbums, setNnumberOfAlbums] = useState([]);
   let numberOfAlbum = numberOfAlbums.length;
+  // functon to get post that own of user
   const getUserPost = async () => {
     await fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
       .then((response) => response.json())
@@ -16,6 +19,7 @@ function UserPost({ userName, userId }) {
         setUserPost(data);
       });
   };
+  // functon to get albums that own of user
   const getUseralbums = async () => {
     await fetch(`https://jsonplaceholder.typicode.com/users/${userId}/albums`)
       .then((response) => response.json())
@@ -24,8 +28,10 @@ function UserPost({ userName, userId }) {
         console.log(numberOfAlbums);
       });
   };
+
   useEffect(getUserPost, []);
   useEffect(getUseralbums, []);
+
   return (
     <div className="UsersPost">
       <p>{userName}</p>
@@ -40,6 +46,6 @@ function UserPost({ userName, userId }) {
       </p>
     </div>
   );
-}
+};
 
 export default UserPost;
